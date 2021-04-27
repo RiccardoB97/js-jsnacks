270573294bd95be14a42c546052e393db913e59c
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-04-27 10:57:24 +0200

    Changed if statement, from longer to shorter word

--- snack-2/assets/js/main.js
+++ snack-2/assets/js/main.js
@@ -2,9 +2,9 @@ firstWord = prompt('Insert a word')
 secondWord = prompt('Insert another word')
 
 if (firstWord.length > secondWord.length) {
-    console.log(firstWord, secondWord);
-} else if (secondWord.length > firstWord.length) {
     console.log(secondWord, firstWord);
+} else if (secondWord.length > firstWord.length) {
+    console.log(firstWord, secondWord);
 } else {
     console.log("both words are long the same")
 }
