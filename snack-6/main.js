480248b465fe866aa092e4c5cdf1873fdebc0c14
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-04-28 12:08:56 +0200

    Calculate the cube of a number where N is given by the user

---
+++ snack-6/main.js
@@ -0,0 +1,8 @@
+// dove N è un numero indicato dall’utente.
+var n = Number(prompt("Insert a number"))
+
+// Stampa il cubo dei primi N numeri,
+
+for (var i = 1; i < n; i++) {
+    console.log(Math.pow(i, 3))
+}
