a7f6b368cf052bca1ad715f71f029519fa3d4afe
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-05-10 12:21:57 +0200

    Fix calculation ipotenuse

--- snack-blocco-4/snack-2/main.js
+++ snack-blocco-4/snack-2/main.js
@@ -6,8 +6,8 @@ var triangle = {
 var area = (triangle.base * triangle.altezza) / 2
 console.log("L'area è " + area + "cm2");
 
-var ipotenusa = Math.sqrt(Math.pow(triangle.altezza, 2) + (Math.pow(triangle.base, 2)));
+var ipotenusa = Math.sqrt(Math.pow(triangle.altezza, 2)) + (Math.pow(triangle.base, 2));
 console.log(ipotenusa);
 
 var perimeter = ipotenusa + triangle.altezza + triangle.base
-console.log("Il perimetro è " + perimeter) + "cm";
+console.log("Il perimetro è " + perimeter + "cm");
