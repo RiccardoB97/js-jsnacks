631dd9999b7777ef844dad47e519a7ad680392ac
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-05-10 11:43:51 +0200

    Add prompt to change weight

--- snack-blocco-4/snack-1/main.js
+++ snack-blocco-4/snack-1/main.js
@@ -1,4 +1,11 @@
+// Snack blocco 4 - 1a
 var palla = {
     nome: 'palla',
     peso: 10
 }
+
+// Snack blocco 4 - 1b
+
+var userPeso = parseInt(prompt('Inserisci il peso della palla: '))
+palla.peso = userPeso
+console.log(palla);
